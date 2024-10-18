47f5e4e9f0cd93f788452469cd6fe9b40a5a2a34
SimpSwapping <154761856+SimpSwapping@users.noreply.github.com>
2024-10-18 03:59:26 +0200

    Dark & light mode Matrix Rain

--- src/components/MatrixRaining.tsx
+++ src/components/MatrixRaining.tsx
@@ -29,165 +29,84 @@ const MatrixRaining = () => {
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
   const isDark = useThemeDetector();
 
-  if (isDark) {
-    useEffect(() => {
-      const canvas = canvasRef.current;
-      if (!canvas) return; // Safety check
-      const ctx = canvas.getContext("2d");
-      if (!ctx) return; // Safety check
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    if (!canvas) return; // Safety check
+    const ctx = canvas.getContext("2d");
+    if (!ctx) return; // Safety check
 
-      let width = (canvas.width = window.innerWidth);
-      let height = (canvas.height = window.innerHeight);
-      let columns = Math.floor(width / 1);
-      const characters = "qwertyuiopasdfghjklzxcvbnm1234567890@#$%&";
-      const charArray = characters.split("");
-      let drops: number[] = [];
+    let width = (canvas.width = window.innerWidth);
+    let height = (canvas.height = window.innerHeight);
+    let columns = Math.floor(width / 1);
+    const characters = "qwertyuiopasdfghjklzxcvbnm1234567890@#$%&";
+    const charArray = characters.split("");
+    let drops: number[] = [];
 
+    for (let i = 0; i < columns; i++) {
+      drops[i] = 1;
+    }
+
+    let frameRate = 30;
+    let lastFrameTime = Date.now();
+
+    // Function to draw the matrix rain effect
+    const draw = () => {
+      ctx.fillStyle = "rgba(10, 10, 10, 0.04)";
+      ctx.fillRect(0, 0, width, height);
+
+      isDark ? (ctx.fillStyle = "#a3a3a3") : (ctx.fillStyle = "#0f0");
+
+      ctx.font = "15px monospace";
+      for (let i = 0; i < drops.length; i++) {
+        const text = charArray[Math.floor(Math.random() * charArray.length)];
+        ctx.fillText(text, i * 20, drops[i] * 20);
+
+        if (drops[i] * 20 > height && Math.random() > 0.975) {
+          drops[i] = 0;
+        }
+
+        drops[i]++;
+      }
+    };
+
+    // Animation loop using requestAnimationFrame
+    const animate = () => {
+      const currentTime = Date.now();
+      const ellapsedTime = currentTime - lastFrameTime;
+
+      if (ellapsedTime > 1000 / frameRate) {
+        draw();
+        lastFrameTime = currentTime;
+      }
+
+      requestAnimationFrame(animate);
+    };
+
+    animate();
+
+    // Function to handle window resizing
+    const handleResize = () => {
+      width = canvas.width = window.innerWidth;
+      height = canvas.height = window.innerHeight;
+      columns = Math.floor(width / 20);
+      drops = [];
       for (let i = 0; i < columns; i++) {
         drops[i] = 1;
       }
+    };
 
-      let frameRate = 30;
-      let lastFrameTime = Date.now();
+    // Window Resizing to prevent issue's on mobile
+    const isMobileDevice = /Mobi/i.test(window.navigator.userAgent);
+    if (!isMobileDevice) {
+      window.addEventListener("resize", handleResize);
+    }
 
-      // Function to draw the matrix rain effect
-      const draw = () => {
-        ctx.fillStyle = "rgba(10, 10, 10, 0.04)";
-        ctx.fillRect(0, 0, width, height);
-
-        ctx.fillStyle = "#a3a3a3";
-
-        ctx.font = "15px monospace";
-        for (let i = 0; i < drops.length; i++) {
-          const text = charArray[Math.floor(Math.random() * charArray.length)];
-          ctx.fillText(text, i * 20, drops[i] * 20);
-
-          if (drops[i] * 20 > height && Math.random() > 0.975) {
-            drops[i] = 0;
-          }
-
-          drops[i]++;
-        }
-      };
-
-      // Animation loop using requestAnimationFrame
-      const animate = () => {
-        const currentTime = Date.now();
-        const ellapsedTime = currentTime - lastFrameTime;
-
-        if (ellapsedTime > 1000 / frameRate) {
-          draw();
-          lastFrameTime = currentTime;
-        }
-
-        requestAnimationFrame(animate);
-      };
-
-      animate();
-
-      // Function to handle window resizing
-      const handleResize = () => {
-        width = canvas.width = window.innerWidth;
-        height = canvas.height = window.innerHeight;
-        columns = Math.floor(width / 20);
-        drops = [];
-        for (let i = 0; i < columns; i++) {
-          drops[i] = 1;
-        }
-      };
-
-      // Window Resizing to prevent issue's on mobile
-      const isMobileDevice = /Mobi/i.test(window.navigator.userAgent);
+    return () => {
       if (!isMobileDevice) {
-        window.addEventListener("resize", handleResize);
+        window.removeEventListener("resize", handleResize);
       }
-
-      return () => {
-        if (!isMobileDevice) {
-          window.removeEventListener("resize", handleResize);
-        }
-      };
-    }, []);
-  } else {
-    useEffect(() => {
-      const canvas = canvasRef.current;
-      if (!canvas) return; // Safety check
-      const ctx = canvas.getContext("2d");
-      if (!ctx) return; // Safety check
-
-      let width = (canvas.width = window.innerWidth);
-      let height = (canvas.height = window.innerHeight);
-      let columns = Math.floor(width / 1);
-      const characters = "qwertyuiopasdfghjklzxcvbnm1234567890@#$%&";
-      const charArray = characters.split("");
-      let drops: number[] = [];
-
-      for (let i = 0; i < columns; i++) {
-        drops[i] = 1;
-      }
-
-      let frameRate = 30;
-      let lastFrameTime = Date.now();
-
-      // Function to draw the matrix rain effect
-      const draw = () => {
-        ctx.fillStyle = "rgba(10, 10, 10, 0.04)";
-        ctx.fillRect(0, 0, width, height);
-
-        ctx.fillStyle = "#0f0";
-
-        ctx.font = "15px monospace";
-        for (let i = 0; i < drops.length; i++) {
-          const text = charArray[Math.floor(Math.random() * charArray.length)];
-          ctx.fillText(text, i * 20, drops[i] * 20);
-
-          if (drops[i] * 20 > height && Math.random() > 0.975) {
-            drops[i] = 0;
-          }
-
-          drops[i]++;
-        }
-      };
-
-      // Animation loop using requestAnimationFrame
-      const animate = () => {
-        const currentTime = Date.now();
-        const ellapsedTime = currentTime - lastFrameTime;
-
-        if (ellapsedTime > 1000 / frameRate) {
-          draw();
-          lastFrameTime = currentTime;
-        }
-
-        requestAnimationFrame(animate);
-      };
-
-      animate();
-
-      // Function to handle window resizing
-      const handleResize = () => {
-        width = canvas.width = window.innerWidth;
-        height = canvas.height = window.innerHeight;
-        columns = Math.floor(width / 20);
-        drops = [];
-        for (let i = 0; i < columns; i++) {
-          drops[i] = 1;
-        }
-      };
-
-      // Window Resizing to prevent issue's on mobile
-      const isMobileDevice = /Mobi/i.test(window.navigator.userAgent);
-      if (!isMobileDevice) {
-        window.addEventListener("resize", handleResize);
-      }
-
-      return () => {
-        if (!isMobileDevice) {
-          window.removeEventListener("resize", handleResize);
-        }
-      };
-    }, []);
-  }
+    };
+  }, []);
 
   return (
     <canvas className='fixed matrix-canvas -z-10' ref={canvasRef}></canvas>
